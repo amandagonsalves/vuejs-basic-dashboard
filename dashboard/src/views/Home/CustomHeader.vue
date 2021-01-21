e37b5c8aa8a8d42b232bb5e2efdf1b972655fedc
Amanda Gonsalves <amandagonsalves75@gmail.com>
2021-01-21 20:58:46 -0300

    update

---
+++ dashboard/src/views/Home/CustomHeader.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>custom header</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
--- dashboard/src/views/Home/index.vue
+++ dashboard/src/views/Home/index.vue
@@ -1,3 +1,41 @@
 <template>
-  <h1 class="font-black text-brand-main">Aqui é a home</h1>
+  <div>
+    <CustomHeader @create-account="handleAccountCreate" @login="handleLogin" />
+    <Contact />
+    <div class="flex justify-center py-10 bg-brand-gray">
+      <p class="font-medium text-center text-gray-800">
+        feedbacker &#169; 2021
+      </p>
+    </div>
+  </div>
 </template>
+
+<script>
+import CustomHeader from "./CustomHeader";
+import Contact from "./Contact";
+import { onMounted } from "vue";
+import { useRouter } from "vue-router";
+
+export default {
+  components: { CustomHeader, Contact },
+  setup() {
+    const router = useRouter();
+    onMounted(() => {
+      const token = window.localStorage.getItem("token");
+
+      if (token) {
+        router.push({ name: 'Feedbacks' });
+      }
+    });
+
+    function handleLogin() {}
+
+    function handleAccountCreate() {}
+
+    return {
+      handleLogin,
+      handleAccountCreate,
+    };
+  },
+};
+</script>
