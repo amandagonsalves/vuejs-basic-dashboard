cb27c2055b065942d8af275aa8df5feda2e1eb06
Amanda Gonsalves <amandagonsalves75@gmail.com>
2021-01-22 15:07:53 -0300

    update

--- dashboard/src/components/ModalLogin/index.vue
+++ dashboard/src/components/ModalLogin/index.vue
@@ -1,13 +1,64 @@
 <template>
-  <div class="flex justify-between">
-    <h1 class="text-4xl font-black text-gray-800">Entre na sua conta</h1>
-    <button @click="close" class="text-4xl text-gray-600">&times;</button>
+  <div>
+    <div class="flex justify-between">
+      <h1 class="text-4xl font-black text-gray-800">Entre na sua conta</h1>
+      <button @click="close" class="text-4xl text-gray-600 focus:outline-none">
+        &times;
+      </button>
+    </div>
+
+    <div class="mt-16">
+      <form @submit.prevent="handleSubmit">
+        <label class="block">
+          <span class="text-lg font-medium text-gray-800">Email</span>
+          <input
+            v-model="state.email.value"
+            :class="{
+              'border-brand-danger': !!state.email.errorMessage
+            }"
+            type="email"
+            class="block w-full px-4 py-3 mt-1 text-lg bg-gray-100 border-2 border-transparent rounded"
+            placeholder="jane.dae@gmail.com"
+          />
+          <span v-if="!!state.email.errorMessage" class="block font-medium text-brand-danger">
+            {{ state.email.errorMessage }}
+          </span>
+        </label>
+
+        <label class="block">
+          <span class="text-lg font-medium text-gray-800">Senha</span>
+          <input
+            v-model="state.password.value"
+            :class="{
+              'border-brand-danger': !!state.password.errorMessage
+            }"
+            type="password"
+            class="block w-full px-4 py-3 mt-1 text-lg bg-gray-100 border-2 border-transparent rounded"
+            placeholder="jane.dae@gmail.com"
+          />
+          <span v-if="!!state.password.errorMessage" class="block font-medium text-brand-danger">
+            {{ state.password.errorMessage }}
+          </span>
+        </label>
+
+        <button
+          :disabled="state.isLoading"
+          type="submit"
+          :class="{
+            'opacity-50': state.isLoading
+          }"
+          class="px-8 py-3 mt-10 text-2xl font-bold text-white rounded-full bg-brand-main focus:outline-none transition-all duration-150"
+        >
+          Entrar
+        </button>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
-import { reactive } from 'vue'
-import useModal from '../../hooks/useModal';
+import { reactive } from "vue";
+import useModal from "../../hooks/useModal";
 export default {
   setup() {
     const modal = useModal();
@@ -16,19 +67,24 @@ export default {
       hasErrors: false,
       isLoading: false,
       email: {
-        value: '',
-        errorMessage: '',
+        value: "",
+        errorMessage: "",
       },
       password: {
-        value: '',
-        errorMessage: '',
+        value: "",
+        errorMessage: "",
       },
     });
 
+    function handleSubmit() {
+
+    }
+
     return {
       state,
-      close: modal.close
-    }
-  }
-}
+      close: modal.close,
+      handleSubmit
+    };
+  },
+};
 </script>
