e0be17bf15285069bc9b5af4a8d2af14cef0e099
Amanda Gonsalves <amandagonsalves75@gmail.com>
2021-01-24 22:34:07 -0300

    update

--- dashboard/src/App.vue
+++ dashboard/src/App.vue
@@ -6,10 +6,31 @@
 </template>
 
 <script>
+import { watch } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
 import ModalFactory from "./components/ModalFactory/index";
+import services from './services/index';
+
 export default {
   components: {
     ModalFactory,
   },
+  setup () {
+    const router = useRouter();
+    const route = useRoute();
+
+    watch(() => route.path, async () => {
+      if (route.meta.hasAuth) {
+        const token = window.localStorage.getItem('token');
+
+        if (!token) {
+          router.push({ name: 'Home' });
+        }
+
+        const { data } = await services.users.getMe();
+        console.log('data', data)
+      }
+    });
+  }
 };
 </script>
