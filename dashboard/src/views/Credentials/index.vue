473566f8e3da7714803b69baa9da50961700a328
Amanda Gonsalves <amandagonsalves75@gmail.com>
2021-01-25 21:01:29 -0300

    update

---
+++ dashboard/src/views/Credentials/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="flex justify-center w-full h-28 bg-brand-main">
+      <HeaderLogged />
+    </div>
+
+    <div class="flex flex-col justify-center items-center h-64 bg-brand-gray">
+      <h1 class="text-4xl font-black text-center text-gray-800">Credenciais</h1>
+      <p class="text-lg font-regular text-center text-gray-800">Guia de instalação e geração de suas credenciais.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import HeaderLogged from "../../components/HeaderLogged/index.vue";
+
+export default {
+  components: {
+    HeaderLogged,
+  },
+};
+</script>
